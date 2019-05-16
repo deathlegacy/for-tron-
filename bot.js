2fbe9dca6236bc849f2387e2d42e49235d0a4dc0
deathlegacy <jessicaciscatan@yahoo.com>
2019-05-17 00:23:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,4 +7,4 @@ const client = new Client(config);
 
 process.on('unhandledRejection', console.error);
 
-client.login(process.env.NTc4NTkwNjE1ODE0Nzk5Mzc3.XN14cg.QwOcNxpsl2cSUnvI-PqmnxLYLuU);
+client.login(process.env.BOT_TOKEN);
